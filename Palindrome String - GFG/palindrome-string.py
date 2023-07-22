d6c76e5df69b9c481b49d35ffe981702c8a70e36
Harsh Mishra <92323449+urstrulyharshx@users.noreply.github.com>
2023-07-22 23:18:47 +0530

    Added solution - LeetHub

---
+++ Palindrome String - GFG/palindrome-string.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+class Solution:
+	def isPalindrome(self, S):
+		# code here
+		rev=S[::-1]
+		if(rev==S):
+		    return 1
+        else:
+            return 0
+		 
+		  
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		S = input()
+		ob = Solution()
+		answer = ob.isPalindrome(S)
+		print(answer)
+
+# } Driver Code Ends

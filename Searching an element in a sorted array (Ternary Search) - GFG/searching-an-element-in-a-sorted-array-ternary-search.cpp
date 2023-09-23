4189d760f86e16550987d9994d3192c5d1ed8346
Harsh Mishra <92323449+urstrulyharshx@users.noreply.github.com>
2023-09-24 01:00:15 +0530

    Added solution - LeetHub

---
+++ Searching an element in a sorted array (Ternary Search) - GFG/searching-an-element-in-a-sorted-array-ternary-search.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    // Function to find element in sorted array
+    // arr: input array
+    // N: size of array
+    // K: element to be searche
+    int ternarySearch(int arr[], int N, int K) 
+    { 
+       // Your code here
+       unordered_map<int,int>mp;
+       for(int i=0;i<N;i++)
+        mp[arr[i]]++;
+        for(int i=0;i<N;i++)
+        {
+            if(mp.find(K)!=mp.end())
+                return 1;
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main(void) 
+{ 
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int N, K;
+        cin >> N >> K;
+        
+        int arr[N];
+        
+        for(int i = 0;i<N;i++){
+            cin >> arr[i];
+        }
+        Solution ob;
+        cout << ob.ternarySearch(arr, N, K) << endl;
+
+    }
+
+	return 0; 
+} 
+
+// } Driver Code Ends
